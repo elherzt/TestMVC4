4357ec72e0a72e11eb241e221ec3f862588d14b2
Jesús Romero <elherzt@gmail.com>
2014-02-21 13:56:44 -0600

    created application

---
+++ TestForGit/TestForGit/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestForGit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Página de descripción de la aplicación.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Página de contacto.";
+
+            return View();
+        }
+    }
+}
